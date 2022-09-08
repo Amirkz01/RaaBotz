c1d53e9d6dc62a4cbabefac853a5fc55e4feb419
Amirkz01 <112914666+Amirkz01@users.noreply.github.com>
2022-09-08 13:18:18 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • Tri [6282154156241]
+│ • Gopay  [6282154156241]
+│ • Dana  [6282154156241]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6282154156241
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
